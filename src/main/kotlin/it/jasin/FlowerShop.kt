5240ceb6d34e61ce535bcef3fbb8731da5dbce4b
Jasin Atipi <jasin@atipi.it>
2023-05-27 15:43:33 +0200

    single orders now handle multiple bundles of the same type

--- src/main/kotlin/it/jasin/FlowerShop.kt
+++ src/main/kotlin/it/jasin/FlowerShop.kt
@@ -4,13 +4,16 @@ import java.math.BigDecimal
 
 class FlowerShop(private val bundles: Bundles) {
     fun generateInvoiceFor(order: String): String {
-        val orderedFlowersAmount = order.split(" ")[0]
+        val orderedFlowersAmount = order.split(" ")[0].toInt()
         val orderedFlowersCode = order.split(" ")[1]
 
         val bundle = bundles
             .available()
-            .first { it.flower.code == orderedFlowersCode && it.amount == orderedFlowersAmount.toInt() }
+            .filter { it.flower.code == orderedFlowersCode }
+            .filter { orderedFlowersAmount % it.amount == 0 }
+            .maxByOrNull { it.amount }
+            ?: return "Invalid order"
 
-        return "$order \$${bundle.usdPrice}: 1 x ${bundle.amount} \$${bundle.usdPrice}"
+        return "$order \$${bundle.usdPrice * BigDecimal(orderedFlowersAmount/bundle.amount)}: ${orderedFlowersAmount/bundle.amount} x ${bundle.amount} \$${bundle.usdPrice}"
     }
 }
--- src/test/kotlin/it/jasin/FlowerShopTest.kt
+++ src/test/kotlin/it/jasin/FlowerShopTest.kt
@@ -1,12 +1,9 @@
 package it.jasin
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
-import kotlin.math.exp
 
 
 class FlowerShopTest {
@@ -17,7 +14,17 @@ class FlowerShopTest {
 
         val invoice = shop.generateInvoiceFor(input)
 
-        assertEquals(invoice, expected)
+        assertEquals(expected, invoice)
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideMultipleSingleTypeBundles")
+    fun `should correctly generate invoice for an order which contains multiple same-number bundles`(input: String, expected: String) {
+        val shop = FlowerShop(Bundles())
+
+        val invoice = shop.generateInvoiceFor(input)
+
+        assertEquals(expected, invoice)
     }
 
     companion object {
@@ -32,6 +39,16 @@ class FlowerShopTest {
                 "3 T58" to "3 T58 $5.95: 1 x 3 $5.95",
                 "5 T58" to "5 T58 $9.95: 1 x 5 $9.95",
                 "9 T58" to "9 T58 $16.99: 1 x 9 $16.99"
+            )
+                .map { Arguments.of(it.first, it.second) }
+        }
+
+        @JvmStatic
+        private fun provideMultipleSingleTypeBundles(): List<Arguments> {
+            return listOf(
+                "50 R12" to "50 R12 $64.95: 5 x 10 $12.99",
+                "15 L09" to "15 L09 $49.75: 5 x 3 $9.95",
+                "6 T58" to "6 T58 $11.90: 2 x 3 $5.95",
             )
                 .map { Arguments.of(it.first, it.second) }
         }
